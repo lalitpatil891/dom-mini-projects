b6ed9952893198ad7264ece758f8fd2bab6447f1
Lalit Patil <lp613408@gmail.com>
2025-06-21 18:25:32 +0530

    Add dynamic Instagram-like story feature with clickable profiles and full-screen story view

--- Instagram Story Feature/script.js
+++ Instagram Story Feature/script.js
@@ -1,5 +1,48 @@
-var arr = [1, 2, 3, 5]
+var arr = [
+    {
+        dp: "https://images.unsplash.com/photo-1750086721456-28c384a8896b?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1741766135837-03019081fc72?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1749223928612-e7f5e9a2211f?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1749898853429-2d7bc67e6433?q=80&w=685&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1743875929006-803ea54cafc1?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1749253811990-7dffe7af8aca?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1749994155472-c0c2e0926642?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1743736750720-5d927199fc15?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNTJ8fHxlbnwwfHx8fHw%3D"
+    },
+];
 
-arr.forEach(function(){
-  console.log("hellow");
+var storiyan = document.querySelector("#storiyan")
+
+var clutter = "";
+arr.forEach(function(elem,idx){
+    clutter += `<div class="story"  >
+          <img id="${idx}" src="${elem.dp}" alt="">
+        </div>`
 })
+
+storiyan.innerHTML = clutter;
+
+storiyan.addEventListener("click", function(dets) {
+       document.querySelector("#full-screen").style.display = "block";
+       document.querySelector("#full-screen").style.backgroundImage = `url(${arr[dets.target.id].story})`
+
+        setTimeout(function(){
+            document.querySelector("#full-screen").style.display = "none"
+        },3000)
+});
+
+// var clutter = ""
+// arr.forEach(function () {
+//     clutter += "Hello"
+// });
+// console.log(clutter)
